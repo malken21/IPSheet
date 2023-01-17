83b4bd5184bdb96892c63feea16cdda4e6978963
malken21 <Malken_Marumasa@outlook.jp>
2023-01-17 22:50:09 +0900

    spigot

--- src/main/java/marumasa/ip_sheet/Config.java
+++ src/main/java/marumasa/ip_sheet/Config.java
@@ -4,11 +4,13 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class Config {
     public final String URL;
+    public final String KickMessage;
 
     public Config(final minecraft plugin) {
         plugin.saveDefaultConfig();
         FileConfiguration config = plugin.getConfig();
 
         URL = config.getString("URL");
+        KickMessage = config.getString("KickMessage");
     }
 }
